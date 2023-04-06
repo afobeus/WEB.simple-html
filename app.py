22039489c3fbc39b8b382d16818b82d248ffbbd1
Artem <artem.dzichkanets@mail.ru>
2023-04-06 19:12:23 +0300

    initial files

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def root():
+    return "Миссия Колонизация Марса"
+
+
+@app.route('/index')
+def index():
+    return "И на Марсе будут яблони цвести!"
+
+
+@app.route('/promotion')
+def promotion():
+    return """Человечество вырастает из детства.<br>
+Человечеству мала одна планета.<br>
+Мы сделаем обитаемыми безжизненные пока планеты.<br>
+И начнем с Марса!<br>
+Присоединяйся!"""
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080)
